cc2397bc45609975671eec60a524b72e595a3b90
atugari <pokemondaisukiyuu7@gmail.com>
2026-09-09 22:43:34 +0900

    Add files via upload

---
+++ maze/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'maze-lab-shell-v1';
+const SHELL = ['./index.html', './manifest.webmanifest', './maze-icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const request = event.request;
+  event.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(response => {
+      if (response.ok && new URL(request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
